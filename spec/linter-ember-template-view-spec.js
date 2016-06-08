23b7728192f88db3e09ad39ff8341cb321aac13e
Ben Holmes <mail@benholmes.uk>
2016-06-07 17:09:39 -0700

    Run generage package

---
+++ spec/linter-ember-template-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LinterEmberTemplateView from '../lib/linter-ember-template-view';
+
+describe('LinterEmberTemplateView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
